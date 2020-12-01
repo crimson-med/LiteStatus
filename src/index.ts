899da712f1a5b8264563c7ccac79b32556adfc2d
crimson-med <mederic.burlet@gmail.com>
2020-12-02 00:37:19 +0800

    :construction: feat(calendar): Working on calendar graph

--- src/index.ts
+++ src/index.ts
@@ -52,6 +52,15 @@ export class WeekGraph {
     }
 }
 
+export function getDaysInMonth(month: number, year: number) {
+    var date = new Date(year, month, 1);
+    var days = [];
+    while (date.getMonth() === month) {
+        days.push(new Date(date));
+        date.setDate(date.getDate() + 1);
+    }
+    return days;
+}
 
 
 function createDiv(divParam: DivParam) {
